31cedc8e62fef4b4d54fedf4f8d5cbaa0d47dffc
SteveOloo <84839553+SteveOloo@users.noreply.github.com>
2021-06-20 10:10:23 +0300

    added comments

--- file_handle.py
+++ file_handle.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 
-
+#Prints a list of all pdf files in the in the given the directory.
 files = [f for f in Path(r'C:\Users\admin\Documents\PDF BOOKS').glob('**/*.pdf')]
 
+
+
 for file in files:
-	name = str(file).split('\\')[-1:][0]
+	# The file output includes the directoy's name plus the file name.
+	# i.e C:\Users\admin\Documents\PDF BOOKS\file_name.pdf
+	name = str(file).split('\\')[-1:][0] # prints the file name and the extenstion alone
 	name_list = name.split()
 
 	print(' '.join(name_list[:-3]))
